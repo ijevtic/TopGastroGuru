ebbeaff34333a73a08c019dc71a17cae371d19b8
Milan Došlić <mdoslic3520rn@raf.rs>
2023-06-24 17:24:07 +0200

    Add MainFragment and new add and replace Fragment methods

--- TopGastroGuru/app/src/main/java/com/example/topgastroguru/presentation/view/activities/MainActivity.kt
+++ TopGastroGuru/app/src/main/java/com/example/topgastroguru/presentation/view/activities/MainActivity.kt
@@ -3,19 +3,17 @@ package com.example.topgastroguru.presentation.view.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.topgastroguru.R
 import com.example.topgastroguru.data.repositories.MealRepositoryImpl
 import com.example.topgastroguru.data.sources.local.MealDao
 import com.example.topgastroguru.data.sources.remote.MealService
 import com.example.topgastroguru.databinding.ActivityMainBinding
 import com.example.topgastroguru.presentation.contract.MealDetaildContract
-import com.example.topgastroguru.presentation.view.activities.fragments.AllMealsFragment
-import com.example.topgastroguru.presentation.view.activities.fragments.MealPlanFragment
-import com.example.topgastroguru.presentation.view.activities.fragments.MyMealsFragment
-import com.example.topgastroguru.presentation.view.activities.fragments.ProfileFragment
-import com.example.topgastroguru.presentation.view.activities.fragments.StatisticsFragment
+import com.example.topgastroguru.presentation.view.activities.fragments.*
 import com.example.topgastroguru.presentation.view.viewmodels.MealDetailedlViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private val mealDetailedVM: MealDetaildContract.ViewModel by viewModel<MealDetailedlViewModel>()
@@ -29,23 +27,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            val fragment: Fragment = when (item.itemId) {
-                R.id.navigation_1 -> AllMealsFragment()
-                R.id.navigation_2 -> MyMealsFragment()
-                R.id.navigation_3 -> MealPlanFragment()
-                R.id.navigation_4 -> StatisticsFragment()
-                R.id.navigation_5 -> ProfileFragment()
-                // Add more cases for additional fragments
-                else -> AllMealsFragment() // Default fragment
-            }
-            replaceFragment(fragment)
-            true
-        }
-        // Set the initial fragment
-        replaceFragment(AllMealsFragment())
-//        val mealDetailedVM= MealDetailedlViewModel(MealRepositoryImpl(MealDao(),MealService())
+        init()
+
+        // Test
         mealDetailedVM.fetchMealById("52772")
+    }
+
+    private fun init () {
+        // Set the initial fragment
+        replaceFragment(MainFragment())
+        initObservers()
+    }
+
+    private fun initObservers(){
+        //TODO: Add observers
+        mealDetailedVM.meal.observe(this, Observer {
+            Timber.e("Meal: $it")
+            addFragment(MealDetailedFragment())
+        })
+    }
+
+    fun addFragment(fragment: Fragment?) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(binding.fragmentContainer.id, fragment!!).addToBackStack(null)
+        transaction.commit()
     }
 
     private fun replaceFragment(fragment: Fragment) {
---
+++ TopGastroGuru/app/src/main/java/com/example/topgastroguru/presentation/view/activities/fragments/MainFragment.kt
@@ -0,0 +1,73 @@
+package com.example.topgastroguru.presentation.view.activities.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.topgastroguru.R
+import com.example.topgastroguru.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            val fragment: Fragment = when (item.itemId) {
+                R.id.navigation_1 -> AllMealsFragment()
+                R.id.navigation_2 -> MyMealsFragment()
+                R.id.navigation_3 -> MealPlanFragment()
+                R.id.navigation_4 -> StatisticsFragment()
+                R.id.navigation_5 -> ProfileFragment()
+                // Add more cases for additional fragments
+                else -> AllMealsFragment() // Default fragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(binding.childFragmentContainer.id, fragment)
+            .commit()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        initUi()
+        initObservers()
+    }
+
+    private fun initUi() {
+
+    }
+
+    private fun initObservers() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
