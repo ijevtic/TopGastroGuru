2b10914f175dcbeb6366490aa7963b14fc7ab676
Ivan Jevtic <ivanjevtic501@gmail.com>
2023-06-25 17:48:48 +0200

    fix

--- TopGastroGuru/app/src/main/java/com/example/topgastroguru/data/models/entities/MealEntity.kt
+++ TopGastroGuru/app/src/main/java/com/example/topgastroguru/data/models/entities/MealEntity.kt
@@ -6,13 +6,13 @@ import java.util.Date
 
 @Entity(tableName = "meal")
 data class MealEntity(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey
     val id:String,
     val name: String,
     val img:String,
     val instructions:String,
     val link:String,
-    val ingredients:Map<String, String>,
+//    val ingredients:Map<String, String>,
 //  Category can be chicken, beef, desert...
     val category: String,
     val date:Date,
--- TopGastroGuru/app/src/main/java/com/example/topgastroguru/presentation/view/viewmodels/AllMealsViewModel.kt
+++ TopGastroGuru/app/src/main/java/com/example/topgastroguru/presentation/view/viewmodels/AllMealsViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.topgastroguru.data.repositories.MealRepository
 
 
-class AllMealsViewModel(
-    private val mealRepository: MealRepository
-): ViewModel(), AllMealsContract.ViewModel {
-}
+//class AllMealsViewModel(
+//    private val mealRepository: MealRepository
+//): ViewModel(), AllMealsContract.ViewModel {
+//}
